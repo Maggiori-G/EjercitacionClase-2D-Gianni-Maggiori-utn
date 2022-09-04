b2946a379595aabcd6f878f9232e532ed3b704e9
Maggiori-G <taniitoo@gmail.com>
2022-09-04 06:31:52 -0300

    More coding vieja

---
+++ Concesionaria/EntidadConcesionaria/Concesionaria.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntidadConcesionaria {
+	public class Concesionaria {
+
+		private string gerente;
+		private string ubicacion;
+		private List <Vehiculo> listaDeAutos;
+		private string numeroDeTelefono;
+		private List<Vehiculo> listaDeAutosVendidos;
+
+		public Concesionaria(string gerente,string ubicacion, string numeroDeTelefono) {
+			this.gerente=gerente;
+			this.ubicacion=ubicacion;
+			this.listaDeAutos= new List<Vehiculo>();
+			this.listaDeAutosVendidos=new List<Vehiculo>();
+			this.numeroDeTelefono=numeroDeTelefono;
+		}
+
+		public string Gerente {
+			get {
+				return this.gerente;
+			}
+		}
+		public string Ubicacion {
+			get {
+				return this.ubicacion;
+			}
+		}
+		public decimal Recaudacion {
+			get {
+				return CalcularRecaudacion(listaDeAutosVendidos);
+			}
+		}
+
+		private bool ConsultarEspacio() {
+			return this.listaDeAutos.Count>0 && this.listaDeAutos.Count <15;
+		}
+		
+		public List<Vehiculo> AgregarVehiculo(List<Vehiculo> listaDeAutos, Vehiculo nuevoVehiculo) {
+			if(ConsultarEspacio() && ComprobarFecha(5,nuevoVehiculo)) {
+				listaDeAutos.Add(nuevoVehiculo);				
+			}
+			return listaDeAutos;
+		}
+		private decimal CalcularRecaudacion(List<Vehiculo> listaDeAutosVendidos) {
+			decimal recaudacion = 0;
+			if(listaDeAutosVendidos is not null) {
+				if(listaDeAutosVendidos.Count()>0) {
+					foreach(Vehiculo item in listaDeAutosVendidos) {
+						recaudacion+=item.Precio;
+					}
+				}
+			}
+			return recaudacion;
+		}
+		private int CalcularDiferenciaAnios(Vehiculo nuevoVehiculo) {
+			DateTime fechaParaComparar=new DateTime();
+			int anioModelo;
+			
+			anioModelo=fechaParaComparar.Year - nuevoVehiculo.FechaDeProduccion.Year;
+			if(fechaParaComparar<nuevoVehiculo.FechaDeProduccion.AddYears(anioModelo)) {
+				anioModelo--;
+			}
+			return anioModelo;
+		}
+		private bool ComprobarFecha(int anios, Vehiculo nuevoVehiculo) {
+			return CalcularDiferenciaAnios(nuevoVehiculo)<anios;
+		}
+		public string MostrarDatosVehiculo(Vehiculo vehiculoParaMostrar) {
+			StringBuilder sb= new StringBuilder();
+			if(vehiculoParaMostrar is not null) {
+				sb.AppendLine($"Marca: {vehiculoParaMostrar.Marca} Fecha del Modelo: {vehiculoParaMostrar.FechaDeProduccion}");
+				if(vehiculoParaMostrar.EsNuevo) {
+					sb.AppendLine($"Estado: Nuevo ");
+				}
+				else {
+					sb.AppendLine($"Estado: Usado ");
+				}
+				if(vehiculoParaMostrar.RuedaDeNieve) {
+					sb.Append($"Las cubiertas estan preparadas para la nieve");
+				}
+				else {
+					sb.Append($"Las cubiertas no estan preparadas para la nieve");
+				}
+				sb.AppendLine($"Precio del vehiculo: {vehiculoParaMostrar.Precio} ");
+				if(vehiculoParaMostrar.Importado) {
+					sb.Append($"El vehiculo es importado");
+				}
+				else {
+					sb.Append($"El vehiculo es nacional");
+				}
+			}
+			return sb.ToString();
+		}
+		private bool ComprobarSiTenemosElVehiuclo(List<Vehiculo> listaDeAutos, Vehiculo vehiculoParaVender) {
+			return listaDeAutos.Contains(vehiculoParaVender);
+		}
+		public bool VenderAuto(List<Vehiculo> listaDeAutos, Vehiculo vehiculoParaVender) {
+			bool seVendio=false;
+			if(listaDeAutos is not null && listaDeAutosVendidos is not null && vehiculoParaVender is not null && ConsultarEspacio()) {
+				if(ComprobarSiTenemosElVehiuclo(listaDeAutos, vehiculoParaVender)) {
+					Console.WriteLine("Se vendio:");
+					Console.WriteLine(MostrarDatosVehiculo(vehiculoParaVender));
+					this.listaDeAutosVendidos.Add(vehiculoParaVender);
+					listaDeAutos.Remove(vehiculoParaVender);
+					seVendio=true;
+				}
+			}
+			return seVendio;
+		}
+		public void MostrarCatalogo(List<Vehiculo> listaDeAutos) {
+			if(listaDeAutos is not null && ConsultarEspacio()) {
+				foreach(Vehiculo item in listaDeAutos) {
+					MostrarDatosVehiculo(item);
+				}
+			}
+		}
+	}
+}
---
+++ Concesionaria/EntidadConcesionaria/Vehiculo.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntidadConcesionaria {
+	public class Vehiculo {
+
+		private string marca;
+		private DateTime fechaDeProduccion;
+		private bool ruedaDeNieve;
+		private bool esNuevo;
+		private Random generadorDePrecio=new Random();
+		private decimal precioDelVehiculo;
+		private bool esImportado;
+
+		public Vehiculo(string marca,DateTime fechaDeProduccion,bool ruedaDeNieve,bool esNuevo) {
+			this.marca=marca;
+			this.fechaDeProduccion=fechaDeProduccion;
+			this.ruedaDeNieve=ruedaDeNieve;
+			this.esNuevo=esNuevo;
+			this.precioDelVehiculo=generadorDePrecio.Next(350000,2000000);
+		}
+		public string Marca {
+			get {
+				return this.marca;
+			}
+		}
+		public DateTime FechaDeProduccion {
+			get {
+				return this.fechaDeProduccion;
+			}
+		}
+		public bool RuedaDeNieve {
+			get {
+				return this.ruedaDeNieve;
+			}
+		}
+		public bool EsNuevo {
+			get {
+				return this.esNuevo;
+			}
+		}
+		public decimal Precio {
+			get {
+				return this.precioDelVehiculo;
+			}
+		}
+		public bool Importado {
+			get {
+				return this.esImportado;
+			}
+		}
+
+		public override bool Equals(object? obj) {
+			return obj is Vehiculo vehiculo&&
+				   marca==vehiculo.marca&&
+				   fechaDeProduccion==vehiculo.fechaDeProduccion&&
+				   ruedaDeNieve==vehiculo.ruedaDeNieve&&
+				   esNuevo==vehiculo.esNuevo&&
+				   EqualityComparer<Random>.Default.Equals(generadorDePrecio,vehiculo.generadorDePrecio)&&
+				   precioDelVehiculo==vehiculo.precioDelVehiculo&&
+				   esImportado==vehiculo.esImportado&&
+				   Marca==vehiculo.Marca&&
+				   FechaDeProduccion==vehiculo.FechaDeProduccion&&
+				   RuedaDeNieve==vehiculo.RuedaDeNieve&&
+				   EsNuevo==vehiculo.EsNuevo&&
+				   Precio==vehiculo.Precio&&
+				   Importado==vehiculo.Importado;
+		}
+
+		public override int GetHashCode() {
+			HashCode hash = new HashCode();
+			hash.Add(marca);
+			hash.Add(fechaDeProduccion);
+			hash.Add(ruedaDeNieve);
+			hash.Add(esNuevo);
+			hash.Add(generadorDePrecio);
+			hash.Add(precioDelVehiculo);
+			hash.Add(esImportado);
+			hash.Add(Marca);
+			hash.Add(FechaDeProduccion);
+			hash.Add(RuedaDeNieve);
+			hash.Add(EsNuevo);
+			hash.Add(Precio);
+			hash.Add(Importado);
+			return hash.ToHashCode();
+		}
+
+		public static bool operator ==(Vehiculo vehiculoUno, Vehiculo vehiculoDos) {
+			return vehiculoUno is not null && vehiculoDos is not null && vehiculoUno.Marca ==vehiculoDos.Marca && vehiculoUno.FechaDeProduccion ==vehiculoDos.FechaDeProduccion;
+		}
+		public static bool operator !=(Vehiculo vehiculoUno, Vehiculo vehiculoDos) {
+			return !(vehiculoUno==vehiculoDos);
+		}
+	}
+}
